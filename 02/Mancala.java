e60acfeeb01229014c11f43965deaa33da2db64a
leite24 <85713750+Leiteduardo@users.noreply.github.com>
2021-07-05 18:48:50 +0000

    Upgrading

--- 02/Mancala.java
+++ 02/Mancala.java
@@ -1,12 +1,14 @@
 import java.io.*;
 import java.util.*;
-/*
-//Peter, Chris, Eduardo
+
+
+
 public class Mancala {
 //  checks if p1-p6 or p7-p12 is empty;
   //Two global varibles
   public static int lastState = 6;
   public static boolean player1LastPlayer = true;
+
   public static int sumS(int[] pits){
     //adds up the seeds in an array.
     int mySum = 0;
@@ -15,6 +17,7 @@ public class Mancala {
     }
     return mySum;
   }
+
   public static Boolean isGameOver(int[] gameBoard){
     Boolean gameOver = false;
     // create slice for p1:p6
@@ -23,21 +26,28 @@ public class Mancala {
     int[] player2Slice = getSliceOfArray( gameBoard, 7, 12);
     int player1NumSeeds = sumS(player1Slice);
     int player2NumSeeds = sumS(player2Slice);
+
     if ( (player1NumSeeds == 0) || (player2NumSeeds == 0) ) {
       gameOver = true;
     } else {
       gameOver = false;
     }
+
     return gameOver;
   }
+
   public static int[] getSliceOfArray(int[] wholeArray, int startIndex, int endIndex) {
+
     int[] slice = new int[endIndex - startIndex + 1];
     int i;
+
     for (i = 0; i < slice.length; i++) {
       slice[i] = wholeArray[startIndex + i];
     }
     return slice;
   }
+
+
   public static int[] startBoard(int[] gameBoard) {
     for (int i = 0; i< gameBoard.length; i++){
       if (i == 6 || i == 13){
@@ -48,6 +58,7 @@ public class Mancala {
     }
     return gameBoard;
   }
+
   public static int[] playerOneTurn(int[] gameBoard){
     //Player one can pick p1-p6. Then the n seeds in the p are sown in the next n places.
     Scanner in = new Scanner(System.in);
@@ -70,7 +81,9 @@ public class Mancala {
       } else {
         //place seed in pit/ player 1's mancala
       gameBoard[modI] = gameBoard[modI] + 1;
+
       }
+
     }
     lastState = modI;
     // When the last stone dropped is into an empty pit on your side,
@@ -79,8 +92,13 @@ public class Mancala {
       gameBoard[6] = gameBoard[6] + gameBoard[12 - lastState]; //Opposite pit has an index of (12 - lastState)
       gameBoard[12 - lastState] = 0;
     }
+
+
     return gameBoard;
   }
+
+
+
   public static int[] playerTwoTurn(int[] gameBoard){
     //Player two can pick p1-p6. Then the n seeds in the p are sown in the next n places.
     Scanner in = new Scanner(System.in);
@@ -103,7 +121,9 @@ public class Mancala {
       } else {
         //place seed in pit/ player 1's mancala
       gameBoard[modI] = gameBoard[modI] + 1;
+
       }
+
     }
     lastState = modI;
     // When the last stone dropped is into an empty pit on your side,
@@ -112,15 +132,21 @@ public class Mancala {
       gameBoard[13] = gameBoard[13] + gameBoard[12 - lastState]; //Opposite pit has an index of (12 - lastState)
       gameBoard[12 - lastState] = 0;
     }
+
     return gameBoard;
+
+
   }
+
   public static void displayBoard(int[] gameBoard){
     //Displays
     System.out.println("Current board: ");
     System.out.println("[Player 1's pits & Mancala; Player 2's pits, and Mancala ]");
     System.out.println("[1, 2, 3, 4, 5, 6, M1; 7, 8, 9, 10, 11, 12, M2]");
     System.out.println(Arrays.toString(gameBoard));
+
   }
+
   public static Boolean playerOneWin(int[] gameBoard) {
     if ( gameBoard[6] > gameBoard[13] ) {
       return true;
@@ -128,6 +154,7 @@ public class Mancala {
       return false;
     }
   }
+
   public static Boolean playerTwoWin(int[] gameBoard) {
     if ( gameBoard[13] > gameBoard[6] ) {
       return true;
@@ -135,6 +162,10 @@ public class Mancala {
       return false;
     }
   }
+
+
+
+
   public static void main(String[] args) {
     /* create a gameBoard array,
     0-5: player one's pits, total of 6 (p1-p6)
@@ -145,7 +176,9 @@ public class Mancala {
     int[] gameBoard;
     gameBoard = new int[14];
     gameBoard = startBoard(gameBoard);
+
     while (!isGameOver(gameBoard)) {
+
       if ((lastState == 6 && player1LastPlayer) ||
           (lastState != 13 && !player1LastPlayer)) {
       gameBoard = playerOneTurn(gameBoard);
@@ -162,6 +195,7 @@ public class Mancala {
         player1LastPlayer = true;
       } else if  ((lastState == 13 && !player1LastPlayer) ||
                    (lastState != 6 && player1LastPlayer)) {
+
         gameBoard = playerTwoTurn(gameBoard);
         if (isGameOver(gameBoard)) {
           if (playerTwoWin(gameBoard)) {
@@ -175,7 +209,12 @@ public class Mancala {
         }
         player1LastPlayer = false;
       }
+
     }
+
     displayBoard(gameBoard);
+
+  }
+}
   }
 }

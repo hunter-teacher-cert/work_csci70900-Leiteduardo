d069a56cfdc8a04ac21ef9f93425499503d7bce1
leite24 <85713750+Leiteduardo@users.noreply.github.com>
2021-07-13 17:29:56 +0000

    01- change suggested by Tiffany

--- DS/Maze/Driver.java
+++ DS/Maze/Driver.java
@@ -1,20 +1,20 @@
-MazeDriver
+  
 import java.io.*;
 import java.util.*;
 
 public class Driver{
 
-    public static void main(String[] args) {
-	boolean solved;
+public static void main(String[] args) {
+boolean solved;
 	
-	Maze m = new Maze("maze.dat");
-	solved = m.solve(1,1);
-	*/
+	//Maze m = new Maze("maze.dat");
+	//System.out.println(m);
+	//solved = m.solve(1,1);
+	
 	
 	Knights k - new Knights(5);
-	  solved = k.solve(1,1);
-	*/
-	Knights k = new Knights(5);
-	k.solve(2,2,1);
-    }
+	solved = k.solve(1,1);
+	
+	
+  }
 }
--- DS/Maze/Maze.java
+++ DS/Maze/Maze.java
@@ -1,16 +1,15 @@
-
 import java.io.*;
 import java.util.*;
 
 public class Maze{
 
-    private char[][] board;
-    private int rows = 25;
-    private int cols = 85;
+  private char[][] board;
+  private int rows = 25;
+  private int cols = 85;
 
-    private String clearScreen="[0;0H\n";
+  private String clearScreen="[0;0H\n";
 
-    private void delay(int n)
+  private void delay(int n)
     {
 	try 
 	    {Thread.sleep(n);} 
@@ -18,13 +17,13 @@ public class Maze{
 	    {}
     }
 
-    public Maze(String filename){
+  public Maze(String filename){
 	try{
 	    board = new char[cols][rows];
 	    int row = 0;
 	    int col = 0;
 
-	    for (row = 0; row < rows; row++){
+	  for (row = 0; row < rows; row++){
 		for (col = 0; col < cols ; col++){
 		    board[col][row] = ' ';
 		}
@@ -37,7 +36,7 @@ public class Maze{
 	    
 	    String line;
 	    row = 0;
-	    while( (line=breader.readLine()) != null )  {
+	  while( (line=breader.readLine()) != null )  {
 		for (col = 0; col < line.length()-1 ; col++){
 		    board[col][row] = line.charAt(col);
 		}
@@ -51,11 +50,11 @@ public class Maze{
     
     }
 
-    public String toString(){
+  public String toString(){
 	int row,col;
 	String result = "";
 	for (row = 0; row < rows; row++){
-	    for (col = 0; col < cols; col++){
+	  for (col = 0; col < cols; col++){
 		result = result + board[col][row];
 	    }
 	    result = result +"\n";
@@ -63,8 +62,38 @@ public class Maze{
 	return result;
     }
 
-    public boolean solve(int col,int row){
-	return false;
+  public boolean solve(int col,int row){
+	boolean solved;
+	
+	System.out.println(clearScreen+this);
+	delay(100);
+	// BASE CASE 1 - solved
+	// if we found the exit we're done - return true
+	if (board[col][row]=='$')
+			       return true;
+
+	// BASE CASE 2 - dead end
+	// if we hit a wall or our path, we can't go further this
+	// way - return false
+	if (board[col][row]==' '
+	    || board[col][row]=='z'
+	    || board[col][row]=='.')
+	    return false;
+
+	// put ourselves in the maze
+	board[col][row]='z';
+	// RECURSIVE CALLS
+	// try all the spaces we can go to 
+	solved = solve(col+1,row);
+	if (!solved)
+	    solved = solve(col-1,row);
+	if (!solved)
+	    solved = solve(col,row+1);
+	if (!solved)
+	    solved = solve(col,row-1);
+	// since we're not done yet remove ourselves
+	board[col][row]='.';
+	return solved;
     }
 
     
